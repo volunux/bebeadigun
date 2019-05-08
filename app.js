4a142972eb00752568e0bb91dbcc859f8c55ce14
Yusuf Musa Ahlal <voludax@gmail.com>
2019-05-08 06:35:53 -0700

    herok

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.use(cookieParser());
 app.use((req, res, next) => {					next(createError(404));										});
 
 
-app.use((err, req, res, next) => {  res.locals.message = err.message;
+app.use((err, req, res, next) => {
+
+console.log(err);
+
+  res.locals.message = err.message;
 																																			res.locals.error = req.app.get('env') === 'development' ? err : {};
 																																																																					res.status(err.status || 500);
 																																																																																					res.render('error');		});
